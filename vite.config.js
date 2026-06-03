12b6401dc7272207bf62c8f5628bfbf9404c33c4
z9877969 <z9877969@ukr.net>
2026-06-03 16:47:39 +0300

    updates sprite sets in vite.config.js + adds example with including sprite to partial

--- vite.config.js
+++ vite.config.js
@@ -45,7 +45,7 @@ export default defineConfig(({ command }) => {
         sort: 'mobile-first',
       }),
       ViteImageOptimizer({
-        exclude: /^sprite.svg$/,
+        exclude: 'sprite.svg',
         png: {
           quality: 60,
         },
